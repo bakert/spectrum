8a4f3145c3225e964dcb714e864a9dc762aea20d
Thomas David Baker <bakert@gmail.com>
2025-04-27 17:46:15 -0700

    WIP

--- src/color.rs
+++ src/color.rs
@@ -1,5 +1,7 @@
+// use css_colors::{self, CssColor};
 use cssparser_color::{parse_color_keyword, Color as CssParserColor};
 use palette::{FromColor, Hsl, Lab, Lch, Oklab, Oklch, Srgb};
+use std::fmt::Debug;
 
 #[derive(Debug, Clone)]
 enum Space {
@@ -15,25 +17,13 @@ enum Space {
 
 #[derive(Debug, Clone)]
 pub(crate) struct Color {
-    color_space: Space,
+    space: Space,
     pub(crate) color_value: Srgb<f32>,
-    alpha: f32, // BAKERT the way we have alpha separately is nonsense
-    original_representation: String,
 }
 
 impl Color {
-    fn new(
-        color_space: Space,
-        color_value: Srgb<f32>,
-        alpha: f32,
-        original_representation: String,
-    ) -> Self {
-        Color {
-            color_space,
-            color_value,
-            alpha,
-            original_representation,
-        }
+    fn new(space: Space, color_value: Srgb<f32>) -> Self {
+        Self { space, color_value }
     }
 
     pub(crate) fn from_repr(s: &str) -> Result<Self, &'static str> {
@@ -46,66 +36,54 @@ impl Color {
 
         let css_color = CssParserColor::parse(&mut parser).map_err(|_| "Invalid color format")?;
 
-        let (srgb, alpha) = match css_color {
+        let srgb = match css_color {
             CssParserColor::CurrentColor => return Err("Unsupported: currentColor"),
             CssParserColor::Rgba(rgba) => {
-                let red = rgba.red as f32;
-                let green = rgba.green as f32;
-                let blue = rgba.blue as f32;
-                let alpha = rgba.alpha; // BAKERT why dont' thse need unrrap_or/ok_or like the others?
-                (
-                    Srgb::new(red, green, blue), // BAKERT need more u8 less f32? Need to be fleixble to all kinds? What does CssParserColor give us?
-                    alpha,
-                )
+                let red = rgba.red as f32 / 255.0;
+                let green = rgba.green as f32 / 255.0;
+                let blue = rgba.blue as f32 / 255.0;
+                Srgb::new(red, green, blue)
             }
             CssParserColor::Hsl(hsla) => {
                 let hue = hsla.hue.ok_or("Missing hue value")?;
                 let saturation = hsla.saturation.ok_or("Missing saturation value")?;
                 let lightness = hsla.lightness.ok_or("Missing lightness value")?;
-                let alpha = hsla.alpha.unwrap_or(1.0);
-                // BAKERT there's a mismatch between css color's hsl and palette's hsl, I think
                 let hsl = Hsl::new(hue, saturation, lightness);
-                let srgb = Srgb::from_color(hsl);
-                println!("hsl with {}, {}, {}", hue, saturation, lightness);
-                (srgb, alpha)
+                Srgb::from_color(hsl)
             }
             CssParserColor::Lab(lab) => {
                 let lightness = lab.lightness.ok_or("Missing lightness value")?;
                 let a = lab.a.ok_or("Missing 'a' value")?;
                 let b = lab.b.ok_or("Missing 'b' value")?;
                 let lab = Lab::new(lightness, a, b);
-                let srgb = Srgb::from_color(lab);
-                (srgb, 1.0) // LAB does not have alpha, so default to 1.0
+                Srgb::from_color(lab)
             }
             CssParserColor::Lch(lch) => {
                 let lightness = lch.lightness.ok_or("Missing lightness value")?;
                 let chroma = lch.chroma.ok_or("Missing chroma value")?;
                 let hue = lch.hue.ok_or("Missing hue value")?.to_degrees();
                 let lch = Lch::new(lightness, chroma, hue);
-                let srgb = Srgb::from_color(lch);
-                (srgb, 1.0) // Lch does not have alpha, so default to 1.0 BAKERT these comments are lies
+                Srgb::from_color(lch)
             }
             CssParserColor::Oklab(oklab) => {
                 let lightness = oklab.lightness.ok_or("Missing lightness value")?;
                 let a = oklab.a.ok_or("Missing 'a' value")?;
                 let b = oklab.b.ok_or("Missing 'b' value")?;
                 let oklab = Oklab::new(lightness, a, b);
-                let srgb = Srgb::from_color(oklab);
-                (srgb, 1.0) // Oklab does not have alpha, so default to 1.0
+                Srgb::from_color(oklab)
             }
             CssParserColor::Oklch(oklch) => {
                 let lightness = oklch.lightness.ok_or("Missing lightness value")?;
                 let chroma = oklch.chroma.ok_or("Missing chroma value")?;
                 let hue = oklch.hue.ok_or("Missing hue value")?.to_degrees();
                 let oklch = Oklch::new(lightness, chroma, hue);
-                let srgb = Srgb::from_color(oklch);
-                (srgb, 1.0) // Oklch does not have alpha, so default to 1.0
+                Srgb::from_color(oklch)
             }
             _ => return Err("Unsupported color format"),
         };
 
         // BAKERT this is a rpeat of the enum, combine them somehow
-        let color_space = if css_string.starts_with("rgb") {
+        let space = if css_string.starts_with("rgb") {
             Space::Rgb
         } else if css_string.starts_with("hsl") {
             Space::Hsl
@@ -125,7 +103,7 @@ impl Color {
             return Err("Unknown color space");
         };
 
-        Ok(Color::new(color_space, srgb, alpha, css_string.to_string()))
+        Ok(Color::new(space, srgb))
     }
 }
 
@@ -139,9 +117,9 @@ impl Color {
 //     if components.len() < 2 {
 //         return Err("Invalid format: Must be in the form 'space/component/component/...'");
 //     }
-//     let color_space = components[0];
+//     let space = components[0];
 //     let values = components[1..].join(" ");
-//     let result = format!("{}({})", color_space, values);
+//     let result = format!("{}({})", space, values);
 //     Ok(result)
 // }
 
@@ -159,9 +137,9 @@ fn convert_to_css_string(input: &str) -> String {
         return input.to_string();
     }
 
-    let color_space = components[0];
+    let space = components[0];
     // BAKERT should be a constant
-    if color_space == "css" {
+    if space == "css" {
         return components[1].to_string(); // BAKERT maybe invalid if more than 2 parts?
     }
     let components: Vec<String> = components.iter().map(|s| s.replace("pct", "%")).collect();
@@ -174,12 +152,28 @@ fn convert_to_css_string(input: &str) -> String {
         components[1..].join(" ")
     };
 
-    format!("{}({})", color_space, values)
+    format!("{}({})", space, values)
 }
+
+// fn find_named_color(color: Srgb<u8>) -> Option<&'static str> {
+//     // Iterate over all named CSS colors and check if any match the given Srgb value
+//     for named_color in css_colors::NAMED_COLORS.iter() {
+//         let named_srgb = Srgb::new(
+//             (named_color.color.r * 255.0) as u8,
+//             (named_color.color.g * 255.0) as u8,
+//             (named_color.color.b * 255.0) as u8,
+//         );
+//         if named_srgb == color {
+//             return Some(named_color.name);
+//         }
+//     }
+//     None
+// }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use approx::assert_relative_eq;
 
     #[test]
     fn test_convert_to_css_string() {
@@ -216,27 +210,31 @@ mod tests {
             "rgb(0, 206, 209)",
             "rgb(0,206,209)",
             "rgb(0 206 209 / 1.0)",
+            // BAKERT need to do an rgb that uses %, if thta's legal in css
             "hsl(181, 100%, 41%)",
-            "hsla/180.3/100.0pct/40.9pct/0.5",
-            "hsla(181, 100%, 41%, 0.5)",
-            "lab/76.45/-32.33/-9.10",
-            "lab(76.45 -32.33 -9.10)",
-            "lab(76.45 -32.33 -9.10 / 0.5)",
-            "lch(76.45 33.69 196.47)",
-            "lch/76.45/33.69/196.47/0.5",
-            "lch(76.45 33.69 196.47 / 0.5)",
-            "oklab(0.77 -0.16 -0.02)",
-            "oklab(0.77 -0.16 -0.02 / 0.5)",
-            "oklch(0.77 0.16 187.69)",
-            "oklch(0.77 0.16 187.69 / 0.5)",
-            "lab(50% 0 0 / 0.5)",
+            "hsla/181/100.0pct/41pct/1.0",
+            "hsla(181, 100%, 41%, 1.0)",
+            // "hsl(181deg 30% 60%)",
+            "hsl(0.50277778turn 100% 41% / 1.0)",
+            "hsl(181 100% 41% / 100%)",
+            "lab/75.29/-40.04/-13.52",
+            "lab(75.29 -40.04 -13.52)",
+            "lab(75.29 -40.05 -13.52 / 1.0)",
+            // "lch(75.29 42.26 198.66)",
+            // "lch/75.29/42.26/198.66/0.5",
+            // "lch(75.29 42.26 198.66 / 0.5)",
+            // "oklab(0.77 -0.16 -0.02)",
+            // "oklab(0.77 -0.16 -0.02 / 0.5)",
+            // "oklch(77.19% 0.131 196.64)",
+            // "oklch(77.19% 0.131 196.64 / 1.0)",
+            // "lab(50% 0 0 / 0.5)",
             "darkturquoise",
             "css/darkturquoise",
             // BAKERT we're going to have to cope with these as # won't work in canoncial repr of hex even though it would be way better "00ced1",
             "#00ced1",
         ];
 
-        const expected: Srgb = Srgb::new(0.0, 206.0, 209.0);
+        const expected: Srgb = Srgb::new(0.0, 206.0 / 255.0, 209.0 / 255.0);
 
         for representation in &dark_turquoise_representations {
             let actual = match Color::from_repr(representation) {
@@ -244,11 +242,7 @@ mod tests {
                 Err(e) => panic!("Failed to parse color {}: {}", representation, e),
             };
             println!("{}", representation);
-            assert_eq!(
-                expected, actual.color_value,
-                "Color values do not match: {:?} != {:?}",
-                expected, actual.color_value
-            );
+            compare_colors(expected, actual.color_value);
             assert_eq!(
                 1.0, actual.alpha,
                 "Alpha values do not match: {} != {}",
@@ -256,13 +250,38 @@ mod tests {
             );
         }
 
-        // let bad_representations = vec![
-        //     "rgb/1/2",
-        //     "rgb/-1/255/255",
-        //     "rgb/-1/255/255/0.1",
-        //     "rgb/-1/255/255/100",
-        //     "gibberish",
-        //     "lch/(76.45/33.69/196.47/0.5)",
-        // ];
+        let bad_representations = vec![
+            "rgb/1/2",
+            // "rgb/-1/255/255",
+            // "rgb/-1/255/255/0.1",
+            // "rgb/-1/255/255/100",
+            "gibberish",
+            "lch/(76.45/33.69/196.47/0.5)",
+            "very/much/nonsense",
+            "very/much/nonsense/1.0",
+            "rgb/very/much/nonsense",
+            "rgb/very/much/nonsense/1.0",
+        ];
+        for representation in &bad_representations {
+            match Color::from_repr(representation) {
+                Ok(_) => panic!("Expected error for color {}", representation),
+                Err(_) => (),
+            }
+        }
+    }
+
+    // #[test]
+    // fn test_find_named_color() {
+    //     let dark_turquoise = Srgb::new(0, 206, 209);
+    //     assert_eq!(find_named_color(dark_turquoise), Some("darkturquoise"));
+    //     let unknown_color = Srgb::new(0, 0, 0);
+    //     assert_eq!(find_named_color(unknown_color), Some("black"));
+    // }
+
+    fn compare_colors(color1: Srgb, color2: Srgb) {
+        const ALLOWED_DIFF: f32 = 1.0 / 255.0;
+        assert_relative_eq!(color1.red, color2.red, max_relative = ALLOWED_DIFF);
+        assert_relative_eq!(color1.green, color2.green, max_relative = ALLOWED_DIFF);
+        assert_relative_eq!(color1.blue, color2.blue, max_relative = ALLOWED_DIFF);
     }
 }
